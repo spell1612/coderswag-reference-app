06580cdb717ed265fb537be87eae2b5950205a5b
spell1612 <spell1612@gmail.com>
2019-02-06 14:16:45 +0530

    Initial Commit

---
+++ coderswag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coderswag
+//
+//  Created by Sayooj Sojen on 06/02/19.
+//  Copyright © 2019 spell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
