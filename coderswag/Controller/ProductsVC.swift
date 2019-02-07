a239724b4b877918cd187c2c53eaa1826a08a60f
spell1612 <spell1612@gmail.com>
2019-02-07 15:45:14 +0530

    minor fixes-first version

--- coderswag/Controller/ProductsVC.swift
+++ coderswag/Controller/ProductsVC.swift
@@ -12,15 +12,17 @@ class ProductsVC: UIViewController, UICollectionViewDataSource,UICollectionViewD
    
 
     @IBOutlet weak var collectionView: UICollectionView!
-    
     @IBOutlet weak var productLabel: UILabel!
-    var catagoryTitle: String!
+    
+    var catagoryTitle: String! //this can also be done by declaring an empty private array of the model and populating it with the data from the object passed in a func.
+    //you can also pass the whole selected/touched object if more than one thing in it has to be used here.
     
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.dataSource=self
         collectionView.delegate=self
         productLabel.text=catagoryTitle
+        navigationItem.title="\(catagoryTitle!) COLLECTION"
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
